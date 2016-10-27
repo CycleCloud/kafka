96a33a89ea7c1e01ca79caa95e2cb146215e50f1
bwatrous <bwatrous@cyclecomputing.com>
2016-10-27 09:51:43 -0400

    Remove unused nopackage cookbook and apt cookbook which already exists in common

--- chef/site-cookbooks/nopackage/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'nopackage'
-maintainer       'Cycle Computing LLC'
-maintainer_email 'bwatrous@cyclecomputing.com'
-license          'All rights reserved'
-description      'Disables the Chef Package resource for all cookbooks in a converge'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
-
